49e61f06df5fef433e1d15c4e59f7d5ec1199ef1
djcloutier <djcloutier@gmail.com>
2022-01-27 12:50:11 -0500

    Update build-iso.sh
    echo dependencies

--- build-iso.sh
+++ build-iso.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+echo "This script requires genisoimage and p7zip-full"
 FILE=focal-live-server-amd64.iso
 if test -f "$FILE"; then
     echo "$FILE exists."
     else
+    echo "downloading source iso..."
     wget -N https://cdimage.ubuntu.com/ubuntu-server/focal/daily-live/current/focal-live-server-amd64.iso
 fi
 
@@ -17,8 +19,9 @@ cp txt.cfg source-files/isolinux/
 cp user-data source-files/server/
 touch source-files/server/meta-data
 cd source-files
+echo "creating iso..."
 genisoimage -quiet -D -r -V "glatt-hypervisor-focal" -cache-inodes -J -l -joliet-long -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -o  ../glatt-hypervisor-focal.iso .
 
 #disable integrity check
 echo > md5sum.txt
-
+echo "done"
