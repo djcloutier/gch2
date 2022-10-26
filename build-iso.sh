10ec0707acaae18f522d6422f30790e9f0728ba0
djcloutier <djcloutier@gmail.com>
2022-10-26 14:07:09 -0400

    Update build-iso.sh
    updated to jammy

--- build-iso.sh
+++ build-iso.sh
@@ -16,13 +16,13 @@ if test -f "$FILE"; then
     echo "$FILE exists."
     else
     echo "downloading source iso..."
-    wget -N https://cdimage.ubuntu.com/ubuntu-server/focal/daily-live/current/focal-live-server-amd64.iso
+    wget -N https://cdimage.ubuntu.com/ubuntu-server/jammy/daily-live/current/jammy-live-server-amd64.iso
 fi
 
 rm -rf source-files
 mkdir source-files
 
-7z -y x focal-live-server-amd64.iso -osource-files
+7z -y x jammy-live-server-amd64.iso -osource-files
 rm -rf source-files/[BOOT]/
 cp grub.cfg source-files/boot/grub/
 mkdir source-files/server
@@ -31,7 +31,7 @@ cp user-data source-files/server/
 touch source-files/server/meta-data
 cd source-files
 echo "creating iso..."
-genisoimage -quiet -D -r -V "glatt-hypervisor-focal" -cache-inodes -J -l -joliet-long -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -o  ../glatt-hypervisor-focal.iso .
+genisoimage -quiet -D -r -V "glatt-hypervisor-jammy" -cache-inodes -J -l -joliet-long -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -o  ../glatt-hypervisor-jammy.iso .
 
 #disable integrity check
 echo > md5sum.txt
